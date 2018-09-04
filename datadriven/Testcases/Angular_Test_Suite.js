9e683303dd1285466a8e3c34c39f6fcb5a92b390
abdullahsohail92 <sohailabdullah92@outlook.com>
2018-09-05 04:30:44 +0500

    Framework updation

--- datadriven/Testcases/Angular_Test_Suite.js
+++ datadriven/Testcases/Angular_Test_Suite.js
@@ -4,38 +4,104 @@ chai.use(require('chai-smoothie'));
 var expect = chai.expect;
 //invoke test data file
 var testData = require('../TestData/AngularTestData.json');
-describe(testData.TestName, () => {
+beforeEach(() => {
+  //waiting for angular to enabled
+  browser.waitForAngularEnabled(false);
+  //Browser URL
+  browser.get(testData[0].url.toString());
 
-  beforeEach(() => {
-    //waiting for angular to enabled
-    browser.waitForAngularEnabled(false);
-    //Browser URL
-    browser.get(testData[0].url.toString());
-
-  });
+});
+//1st testcase
+describe(testData[0].TestName, function() {
 
   var Angular_HomePage = require('../pageObject/AngularJsHome.js')
-  //1st testcase
-  it('Testcase ID:' + testData[0].TestcaseId + ': ' + testData[0].TestStepDescription, () => {
+  //Step 1
+  it('Step#:' + testData[0].TestcaseId + ': ' + testData[0].TestStepDescription, () => {
     Angular_HomePage.NameTextBox(testData[0].name);
     var getHomepageText = Angular_HomePage.getDynamicText();
     //expect(getHomepageText).toBe(testData[0].nameAssertion);
     expect(getHomepageText).to.have.text(testData[0].nameAssertion)
   });
 
-  //2nd testcase
-  it('Testcase ID:' + testData[1].TestcaseId + ': ' + testData[1].TestStepDescription, () => {
+  //Step 2
+  it('Step#:' + testData[1].TestcaseId + ': ' + testData[1].TestStepDescription, () => {
     var listItem = Angular_HomePage.MatchListValues(1);
     //expect(listItem).toBe(testData[0].ListItemText);
     expect(listItem).to.have.text(testData[0].ListItemText)
   });
 
-  //3rd testcase
-  it('Testcase ID:' + testData[2].TestcaseId + ': ' + testData[2].TestStepDescription, () => {
-    var res=expect(element(by.css('h1'))).to.be.present;   
-    
-   
+  //Step 3
+  it('Step#:' + testData[2].TestcaseId + ': ' + testData[2].TestStepDescription, () => {
+    var res = expect(element(by.css('h1'))).to.be.present;
+
+  });
+  //Step 4
+  it('Step#:' + testData[3].TestcaseId + ': ' + testData[3].TestStepDescription, () => {
+
+
   });
 
-});
+}),
+  //2nd testcase
+  xdescribe(testData[1].TestName, () => {
+
+    var Angular_HomePage = require('../pageObject/AngularJsHome.js')
+    //Step 1
+    it('Step#:' + testData[0].TestcaseId + ': ' + testData[0].TestStepDescription, () => {
+      Angular_HomePage.NameTextBox(testData[0].name);
+      var getHomepageText = Angular_HomePage.getDynamicText();
+      //expect(getHomepageText).toBe(testData[0].nameAssertion);
+      expect(getHomepageText).to.have.text(testData[0].nameAssertion)
+    });
+
+    //Step 2
+    it('Step#:' + testData[1].TestcaseId + ': ' + testData[1].TestStepDescription, () => {
+      var listItem = Angular_HomePage.MatchListValues(1);
+      //expect(listItem).toBe(testData[0].ListItemText);
+      expect(listItem).to.have.text(testData[0].ListItemText)
+    });
+
+    //Step 3
+    it('Step#:' + testData[2].TestcaseId + ': ' + testData[2].TestStepDescription, () => {
+      var res = expect(element(by.css('h1'))).to.be.present;
+
+    });
+    //Step 4
+    it('Step#:' + testData[3].TestcaseId + ': ' + testData[3].TestStepDescription, () => {
+
+
+    });
+
+  }),
+  //3rd testcase
+  xdescribe(testData[2].TestName, () => {
+
+    var Angular_HomePage = require('../pageObject/AngularJsHome.js')
+    //Step 1
+    it('Step#:' + testData[0].TestcaseId + ': ' + testData[0].TestStepDescription, () => {
+      Angular_HomePage.NameTextBox(testData[0].name);
+      var getHomepageText = Angular_HomePage.getDynamicText();
+      //expect(getHomepageText).toBe(testData[0].nameAssertion);
+      expect(getHomepageText).to.have.text(testData[0].nameAssertion)
+    });
+
+    //Step 2
+    it('Step#:' + testData[1].TestcaseId + ': ' + testData[1].TestStepDescription, () => {
+      var listItem = Angular_HomePage.MatchListValues(1);
+      //expect(listItem).toBe(testData[0].ListItemText);
+      expect(listItem).to.have.text(testData[0].ListItemText)
+    });
+
+    //Step 3
+    it('Step#:' + testData[2].TestcaseId + ': ' + testData[2].TestStepDescription, () => {
+      var res = expect(element(by.css('h1'))).to.be.present;
+
+    });
+    //Step 4
+    it('Step#:' + testData[3].TestcaseId + ': ' + testData[3].TestStepDescription, () => {
+
+
+    });
+
+  });
 
